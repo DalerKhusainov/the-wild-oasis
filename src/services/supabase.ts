2282e61a81f6dc4c9215992f1a3bcf55bcc11f4c
DalerKhusainov <dalerkhusainov85@gmail.com>
2026-01-06 23:33:35 +0500

    finished creating tables in supabase

---
+++ src/services/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://vkyllgwuoytggeelqmto.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZreWxsZ3d1b3l0Z2dlZWxxbXRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2MzM3MjAsImV4cCI6MjA4MzIwOTcyMH0.3NRdPSjpC3BxpI3lwI1tZURj2S2ahvMmbDZN7PCUoSI";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
