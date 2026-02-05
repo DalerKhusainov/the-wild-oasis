90d965c546b6104aec1e15316e37ab4172cb897f
DalerKhusainov <dalerkhusainov85@gmail.com>
2026-02-05 20:21:32 +0500

    finished authorization and protected route

---
+++ src/ui/ProtectedRoute.tsx
@@ -0,0 +1,41 @@
+import { ReactNode, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../features/authentication/useUser";
+import { styled } from "styled-components";
+import Spinner from "./Spinner";
+
+const FullPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: var(--color-grey-50);
+`;
+
+function ProtectedRoute({ children }: { children: ReactNode }) {
+  const navigate = useNavigate();
+
+  // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 2. If there is NO authenticated user, redirect to the /login
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/login");
+  }, [isAuthenticated, isLoading, navigate]);
+
+  // 3. While loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  console.log(!isAuthenticated, !isLoading);
+  console.log(isAuthenticated);
+
+  // 4. If there IS a user, render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
